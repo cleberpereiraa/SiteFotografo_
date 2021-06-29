40ea36827135ca7e8613b397a81a6de9dc69200a
Cleber da Silva Pereira <cleberpereira100@gmail.com>
2021-06-29 01:38:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,9 +17,38 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+/********************************************************** */
+function clicou1(){
+    var img = document.getElementById("ges");
+    img.style.width = "500px"          /*testeeee*////
+}
+function saiu1(){
+    let img = document.getElementById("ges");
+    img.style.width = "350px"
+}
 
+function clicou2(){
+    let img = document.getElementById("pre");
+    img.style.width = "500px"          /*testeeee*////
+}
+function saiu2(){
+    let img = document.getElementById("pre");
+    img.style.width = "350px"
+}
 
-function clicou(){
-    var img = document.getElementById("img2");
-    img.innerHTML= ""          /*testeeee*////
+function clicou3(){
+    let img = document.getElementById("casal");
+    img.style.width = "500px"          /*testeeee*////
+}
+function saiu3(){
+    let img = document.getElementById("casal");
+    img.style.width = "350px"
+}
+function clicou4(){
+    let img = document.getElementById("ani");
+    img.style.width = "500px"          /*testeeee*////
+}
+function saiu4(){
+    let img = document.getElementById("ani");
+    img.style.width = "350px"
 }
